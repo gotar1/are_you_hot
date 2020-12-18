44a029f49aeb28b28bfdb8b0e70c27ea9ccc3e02
Gotar <eltahir.g@gmail.com>
2020-12-17 21:50:47 -0600

    updated files with working model

--- Project_3_Hot_Model/flask_app.py
+++ Project_3_Hot_Model/flask_app.py
@@ -1,3 +1,4 @@
+# import modules
 from flask import Flask, render_template, redirect, request
 from sklearn.feature_extraction.text import CountVectorizer
 from xgboost import XGBClassifier
@@ -10,26 +11,17 @@ import os
 # Create an instance of Flask
 app = Flask(__name__)
 
+# upload model file
 with open(f'./Project_3_Hot_Model/lgbm_model.pickle', "rb") as f:
     model = pickle.load(f)
 
-# with open(f'./Project_3_Hot_Model/xgb_model.pickle', "rb") as f:
-#     model = pickle.load(f)
-
-# model_columns = model.columns
-# filename = './Project_3/lgbm_model.pickle'
-# with open('filename', "rb") as f:
-#     model = pickle.load(f)
-
+# grap feature names from our model
 feature_names = model.booster_.feature_name()
-# feature_names = model.get_booster().feature_names
-# print(feature_names)
 
 # Route to render index.html template
 @app.route("/", methods=["GET", "POST"])
 def home():
     output_message = ""
-    # cv = CountVectorizer()
     if request.method == "POST":
         sex = str(request.form["sex"])
         eye_color = str(request.form["eye color"])
@@ -39,33 +31,26 @@ def home():
         ratio = weight/height
         age = float(request.form["age"])
 
-        # , columns=['sex', 'eye_color', 'distinctive_features', 'zodiac_sign', 'ratio(wt/ht', 'age']
-        # , columns=feature_names
-        # data must be converted to df with matching feature names before predict
+        # convert user input into a dataFrame and let equalize to our data columns..
         data_df = pd.DataFrame(np.array([[sex, eye_color, distinctive_features, ratio, age]]), columns=['sex', 'eye_color', 'distinctive_features', 'ratio(wt/ht)', 'age'])
         data_df['age'] = pd.to_numeric(data_df['age'])
         data_df['ratio(wt/ht)'] = pd.to_numeric(data_df['ratio(wt/ht)'])
         print(data_df)
         print(data_df.dtypes)
-        print(sex)
-        print(eye_color)
-        print(distinctive_features)
-        print(height)
-        print(weight)
-        print(age)
-        data = pd.get_dummies(data_df)
-        print(data.columns)
 
+        data = pd.get_dummies(data_df)
+
+        # use for loop to use only features corresponding to user input.
         for col in feature_names:
             if col not in data.columns:
                 data[col] = 0
-            
+
+        # make our prediction   
         result = model.predict(data[feature_names])
         if result == 0:
             output_message = "Hot Hot..It's Getting Hot in Here ^_^"
         else:
             output_message = "Looks Like You Have a Good Personality :-("
-        # print(output_message)
         print(data)
         print(result)
         print(feature_names)
